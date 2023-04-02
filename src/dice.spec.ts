554ff6222c833e6ba87f8d309849e8acc245c15f
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-04-02 21:44:49 +0100

    dice roll specs

---
+++ src/dice.spec.ts
@@ -0,0 +1,11 @@
+
+
+describe('dice', () => {
+    // dice roll returns number between 1 and 6
+
+    // dice roll number is always positive
+
+    // when dice rolls X, player token can be advanced by X spaces
+
+    // 2000 dice rolls all roll numbers between 1 and 6
+});
