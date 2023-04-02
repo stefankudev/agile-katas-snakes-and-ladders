b21cef1f07d47b0572835c352428d47f7887ed3b
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-04-02 22:13:30 +0100

    game logic, sorta

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("hello world")
+import { Dice } from './dice'
+import { Token } from './token'
+
+const Game = () => {
+    const dice: Dice = new Dice();
+    const token: Token = new Token();
+
+    console.info(`Welcome to 🐍🪜 Snakes and Ladders 🪜🐍`)
+
+    while (token.position < 100) {
+        console.info("Rolling the dice...")
+        const roll: number = dice.roll();
+        console.info(`You rolled ${roll}!`)
+        token.move(roll)
+        console.info(`You are now in position ${token.position} on the board.`)
+    }
+
+    if (token.position === 100) {
+        console.info("🎉 Congratulations! You win! 🎉")
+
+        console.info("Press Enter to start a new game, or Esc to exit.")
+    }
+
+    return;
+}
+
+Game();
