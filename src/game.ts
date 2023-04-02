285a12c13668cdff5ad78daa091b04c82c2f3f18
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-04-02 23:05:22 +0100

    this is cool and all but I have to mock `readline` input and output

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,4 @@
+const readline = require('readline');
 import { Dice } from './dice';
 import { Token } from './token';
 
@@ -16,8 +17,29 @@ export class Game {
         return icons[roll - 1] || '';
     }
 
-    play(): void {
+    private startGameLoop(): void {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+        rl.on('line', (input: unknown) => {
+            console.log(input);
+            // if (input === '') {
+            //     // User pressed Enter
+            //     this.play();
+            //     rl.close();
+            // }
+        });
+
+        rl.on('SIGINT', () => {
+            // User pressed Ctrl-C or Esc
+            rl.close();
+        });
+
         while (this.token.position < 100) {
+            console.info('\n🎲 Press Enter to roll the dice...');
+
             console.info('\n🎲 Rolling the dice...');
             const roll: number = this.dice.roll();
             console.info(`✨ You rolled ${this.getRollIcon(roll)} (${roll})!`);
@@ -29,8 +51,12 @@ export class Game {
             console.info('\n🎉 Congratulations! You win! 🎉\nPress Enter to start a new game, or Esc to exit.\n');
         }
 
+
+    }
+
+    play(): void {
+        const game = new Game();
+        game.startGameLoop();
     }
 }
 
-
-}
