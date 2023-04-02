4f6d4c37d5e513098ce10a36b08535938d8685f5
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-04-02 21:20:07 +0100

    write spec for `token`

---
+++ src/token.spec.ts
@@ -0,0 +1,15 @@
+describe('token', () => {
+    // new token starts at position 1
+
+    // new token moved 3 spaces is on position 4
+
+    // new token moved 3 spaces, then 4 spaces, is in position 8
+
+    // new token moved X spaces is in position 1+X
+
+    // token can be moved forward
+
+    // token can be moved backward
+
+    // token can not be moved past position 100
+});
