31d4718a21a946c8a12bf80c0cff648f8de7631f
Stefan Kudev <19394245+stefankudev@users.noreply.github.com>
2023-04-02 21:38:20 +0100

    move `token` into its own file

--- src/token.spec.ts
+++ src/token.spec.ts
@@ -1,24 +1,4 @@
-class Token {
-    private _position: number;
-
-    constructor(position: number = 1) {
-        this._position = position;
-    }
-
-    get position(): number {
-        return this._position;
-    }
-
-    move(distance: number) {
-        if (this._position + distance > 100) {
-            return;
-        }
-        if (this._position + distance < 1) {
-            return;
-        }
-        this._position += distance;
-    }
-}
+import { Token } from './token'
 
 describe('token', () => {
     test('token starts in position 1 by default', () => {
